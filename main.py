f24c44efc2c94ac679199ddbda0035c2846c43b2
Antti Savolainen <antti.savo@gmail.com>
2026-03-19 22:09:43 +0200

    Simplify builtin functions and add failsafe kill

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-import asyncio
 import signal
 from datetime import datetime
-
 import subprocess
+
 import decky
 
 
@@ -43,39 +42,27 @@ class Plugin:
     return Plugin._process != None
 
 
+
+
+
+
+
   # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
   async def _main(self):
-      self.loop = asyncio.get_event_loop()
-      decky.logger.info("Hello World!")
+    decky.logger.info("Plugin loaded!")
 
   # Function called first during the unload process, utilize this to handle your plugin being stopped, but not
   # completely removed
   async def _unload(self):
-      decky.logger.info("Goodnight World!")
-      pass
+    # Ensure that there are no detached processes
+    subprocess.run(["killall", "gst-launch-1.0"])
+    decky.logger.info("Plugin unloaded!")
 
   # Function called after `_unload` during uninstall, utilize this to clean up processes and other remnants of your
   # plugin that may remain on the system
   async def _uninstall(self):
-      decky.logger.info("Goodbye World!")
-      pass
+    decky.logger.info("Plugin uninstalled!")
 
   # Migrations that should be performed before entering `_main()`.
   async def _migration(self):
-      decky.logger.info("Migrating")
-      # Here's a migration example for logs:
-      # - `~/.config/decky-template/template.log` will be migrated to `decky.decky_LOG_DIR/template.log`
-      decky.migrate_logs(os.path.join(decky.DECKY_USER_HOME,
-                                              ".config", "decky-template", "template.log"))
-      # Here's a migration example for settings:
-      # - `~/homebrew/settings/template.json` is migrated to `decky.decky_SETTINGS_DIR/template.json`
-      # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky.decky_SETTINGS_DIR/`
-      decky.migrate_settings(
-          os.path.join(decky.DECKY_HOME, "settings", "template.json"),
-          os.path.join(decky.DECKY_USER_HOME, ".config", "decky-template"))
-      # Here's a migration example for runtime data:
-      # - `~/homebrew/template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-      # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-      decky.migrate_runtime(
-          os.path.join(decky.DECKY_HOME, "template"),
-          os.path.join(decky.DECKY_USER_HOME, ".local", "share", "decky-template"))
+    decky.logger.info("Migrating")
