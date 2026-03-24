dd9fd9f0627bf89e7aed02a216cec73979832e27
YellowCourtesy <270474062+YellowCourtesy@users.noreply.github.com>
2026-03-24 18:37:29 -0400

    Add rate control to the encoder to reduce file size

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Plugin:
   async def start_record(self, app_name: str, microphone: bool):
     # Generate a gstreamer pipeline
     gstreamer = f"GST_PLUGIN_PATH={decky.DECKY_PLUGIN_DIR}/bin/gstreamer-1.0 gst-launch-1.0 "
-    videopipeline = "pipewiresrc do-timestamp=true target-object=gamescope client-name=Video-capture ! videoconvert ! vah264enc ! h264parse ! mux. "
+    videopipeline = "pipewiresrc do-timestamp=true target-object=gamescope client-name=Video-capture ! videoconvert ! vah264enc rate-control=vbr bitrate=5000 target-percentage=60 ! h264parse ! mux. "
     audiosource = "pipewiresrc do-timestamp=true stream-properties=props,stream.capture.sink=true client-name=Speaker-capture ! audio/x-raw,channels=2 ! mixer. "
     if microphone:
       audiosource = audiosource + "pipewiresrc do-timestamp=true client-name=Microphone-capture ! audio/x-raw,channels=2 ! mixer. "
